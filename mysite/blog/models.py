8c466fc5b4a15dc3905bb625e48d70f68e8b1aae
pravintp <94511854+pravintp@users.noreply.github.com>
2021-12-08 16:15:08 +0530

    Revert "Create custom manager for published questions (#4)" (#27)
    This reverts commit 37b2d46bcff9384f4f0cbb2bbc520bdd31e553ff.

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,11 +3,6 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
-class PublishedManager(models.Manager):
-    def get_queryset(self):
-        return super(PublishedManager, self).get_queryset().filter(status="published")
-
-
 class Post(models.Model):
     STATUS_CHOICES = (
         ("draft", "Draft"),
@@ -24,8 +19,6 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
-    objects = models.Manager()
-    published = PublishedManager()
 
     class Meta:
         ordering = ("-publish",)
