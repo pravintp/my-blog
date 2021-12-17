875708a60a84fd37ce6ca41b8406045fe0372de2
pravintp <94511854+pravintp@users.noreply.github.com>
2021-12-17 10:43:19 +0530

    Add sitemap support to the site (#17)

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,8 +32,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
+SITE_ID = 1
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "blog.apps.BlogConfig",
     "taggit",
+    "django.contrib.sites",
+    "django.contrib.sitemaps",
 ]
 
 MIDDLEWARE = [
