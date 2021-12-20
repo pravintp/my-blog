db05d98f0c9f1e7dc319356d48266f9722f9dbda
pravintp <94511854+pravintp@users.noreply.github.com>
2021-12-20 11:02:14 +0530

    Add stemming and ranking based search for the site (#20)

--- mysite/blog/utils.py
+++ mysite/blog/utils.py
@@ -1,5 +1,5 @@
 from django.core.mail import send_mail
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 from .models import Post
 
@@ -12,6 +12,12 @@ def share_post_by_email(post, name, share_text, to_mail):
 
 
 def get_search_results(query):
-    return Post.published.annotate(
-        search=SearchVector("title", "body"),
-    ).filter(search=query)
+    search_vector = SearchVector("title", "body")
+    search_query = SearchQuery(query)
+    return (
+        Post.published.annotate(
+            search=search_vector, rank=SearchRank(search_vector, search_query)
+        )
+        .filter(search=search_query)
+        .order_by("-rank")
+    )
